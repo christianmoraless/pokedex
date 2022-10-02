d01a2c4ae9faf7d3ff69373380b9fbb6b6b6f7bc
christian morales <christiandmoraless@gmail.com>
2022-10-02 18:30:49 -0400

    end of commits

---
+++ pages/name/[name}.tsx
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { Button, Card, Container, Grid, Text } from '@nextui-org/react';
+import { useRouter } from 'next/router'
+import confetti from 'canvas-confetti';
+import { pokeApi } from '../../api';
+import { Layout } from '../../components/layouts'
+import { Pokemon, PokemonListResponse } from '../../interfaces';
+import { localFavorites } from '../../utils';
+
+interface Props {
+    pokemon:Pokemon;
+}
+
+export const PokemonByNamePage:NextPage<Props> = ({pokemon}) => {
+
+    console.log({pokemon})
+
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id);
+    setIsInFavorites(!isInFavorites);
+
+    if(isInFavorites) return;
+
+    confetti({
+      zIndex:999,
+      particleCount:100,
+      spread:160,
+      angle:-100,
+      origin:{
+        x:1,
+        y:0
+      }
+    })
+
+  }
+  const [isInFavorites, setIsInFavorites] = useState(localFavorites.exitsPokemonInFavorites(pokemon.id)); 
+
+
+    const router = useRouter();
+    return (
+        <Layout title={`${pokemon.name}`}>
+            <Grid.Container css={{marginTop:'5px'}} gap={2}>
+            <Grid xs={12} sm={4}>
+                <Card isHoverable css={{padding:'30px'}}>
+                <Card.Body>
+                    <Card.Image 
+                    src={pokemon.sprites.other?.dream_world.front_default || 'no-image'} 
+                    alt={pokemon.name}
+                    width="100%"
+                    height={200}/>
+                </Card.Body>
+                </Card>
+            </Grid>
+
+            <Grid xs={12} sm={8}>
+                <Card>
+                <Card.Header css={{display:'flex', justifyContent:'space-between'}}>
+                    <Text h1 transform='capitalize'>{pokemon.name}</Text>
+                    <Button color='gradient' ghost={!isInFavorites} onPress={onToggleFavorite}>
+                    {isInFavorites ? 'En Favoritos' : 'Guardar en Favoritos'}
+                    </Button>
+                </Card.Header>
+
+                <Card.Body>
+                    <Text size={30}>Sprites:</Text>
+                    <Container direction='row' display='flex'>
+                    <Card.Image 
+                        src={pokemon.sprites.front_default}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                    />
+                    <Card.Image 
+                        src={pokemon.sprites.back_default}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                    />
+                    <Card.Image 
+                        src={pokemon.sprites.front_shiny}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                    />
+                    <Card.Image 
+                        src={pokemon.sprites.back_shiny}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                    />
+                    </Container>
+                </Card.Body>
+                </Card>
+            </Grid>
+            </Grid.Container>
+        </Layout>
+    )
+}
+
+
+export async function getStaticPaths() {
+    const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+    const pokemonNames:string[] = data.results.map(pokemon => pokemon.name);
+    return {
+      paths: pokemonNames.map(name => ({
+        params: {name}
+      })),
+      fallback: false, 
+    }
+}
+
+export const getStaticProps:GetStaticProps = async ({params}) => {
+    const {name} = params as {name:string};
+    const {data} = await pokeApi.get<Pokemon>(`/pokemon/${name}`);
+
+    const pokemon = {
+        id:data.id,
+        name:data.name,
+        sprites:data.sprites
+    }
+
+    return{
+      props:{
+        pokemon
+      }
+    }
+}
+
+export default PokemonByNamePage
